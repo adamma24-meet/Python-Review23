1670a86d45ae2d174d54ce16e3ba00702827fd0c
adamma24-meet <93448116+adamma24-meet@users.noreply.github.com>
2023-07-17 15:38:22 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,17 @@
+
+
+def creat_youtube_video(title,description):
+	likes=0
+	dislike=0
+	username=""
+	comment=""
+
+	comments={username:comment}
+	videos={'Title':title,'Description':description,'Likes':likes,'Dislike':dislike,'comments': comments}
+	return videos
+x =creat_youtube_video('hhh','ttr')
+
+
+	
+
+
