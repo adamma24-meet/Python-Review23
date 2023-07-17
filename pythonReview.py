657d03172603354db504696455ae35c82c221595
adamma24-meet <93448116+adamma24-meet@users.noreply.github.com>
2023-07-17 18:09:57 +0300

    done

--- pythonReview.py
+++ pythonReview.py
@@ -6,10 +6,33 @@ def creat_youtube_video(title,description):
 	username=""
 	comment=""
 
-	comments={username:comment}
+	comments={}
 	videos={'Title':title,'Description':description,'Likes':likes,'Dislike':dislike,'comments': comments}
 	return videos
-x =creat_youtube_video('hhh','ttr')
+
+def like(x):
+	if "Likes" in x:
+		x['Likes']+=1
+		return x
+
+def dislike(y):
+	if "Dislike" in y:
+		y['Dislike']+=1
+	return y
+
+def add_comment(youtubevideo,username,comment_text):
+	youtubevideo['comments'][username]=comment_text
+
+test =creat_youtube_video('adam','who is adam')
+
+like(test)
+dislike(test)
+add_comment(test,'galegor the destroyer','i am fatter')
+print(test)
+
+		
+
+
 
 
 	
